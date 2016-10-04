e9bf1d07f6ec96a0e36461de173f757a0b759eaf
Florian Dohrendorf <fdohrendorf@outlook.com>
2016-10-04 17:51:58 +0200

    svg example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: '[eco-block]',
+  template: `
+<svg:rect width="300" height="200" style="fill:rgb(0,0,255)"
+[style.fill]="color"
+ (mousemove)="mousemove($event)" />
+`
+})
+export class BlockComponent implements OnInit {
+  color: string;
+
+  constructor() { }
+
+  mousemove(e: MouseEvent) {
+    this.color = 'rgb(' + (e.clientY % 256) + ',' + ((e.clientX + e.clientY) % 256) + ',' + (e.clientX % 256) + ')';
+  }
+
+  ngOnInit() { }
+}
+
+@Component({
+  selector: 'eco-canvas',
+  template: `
+  <svg viewBox="0 0 900 500">
+  <svg:g eco-block />
+  </svg>
+  `
+})
+export class CanvasComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,3 +46,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { AppComponent, CanvasComponent, BlockComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CanvasComponent, BlockComponent
   ],
   imports: [
     BrowserModule,
